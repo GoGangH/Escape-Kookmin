ea94cc1fe0f043282ed3ca7b3c1ead5216fb68ff
minjj0905 <kmj2967@kookmin.ac.kr>
2019-12-15 05:35:59 +0900

    프롤로그 수정, 사운드 볼륨조절 추가

--- 탈국민/sound.py
+++ 탈국민/sound.py
@@ -1,15 +1,16 @@
 from settings import *
 from os import path
 import pygame as pg
-def set_music(filename):
+def set_music(filename, volume=0.5):
     '''bgm을 설정. 인자는 파일명'''
     bgm_folder = path.join(GAME_DIR, 'bgm')
     pg.mixer.music.load(path.join(bgm_folder, filename))
-    pg.mixer.music.set_volume(0.3)
+    pg.mixer.music.set_volume(volume)
     pg.mixer.music.play(-1)
 
-def set_sfx(filename):
-    '''효과음을 설정. 인자는 파일명'''
+def set_sfx(filename, volume=0.5):
+    '''효과음을 설정. 인자는 파일명, 볼륨'''
     sfx_folder = path.join(GAME_DIR, 'sfx')
     effect = pg.mixer.Sound(path.join(sfx_folder, filename))
+    effect.set_volume(volume)
     effect.play()
